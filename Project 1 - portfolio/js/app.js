acddf991fab0b2a34c1859983e0eb6f1dc02ef93
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-05-23 14:09:05 +0500

    Add Hemberger menu on website

--- Project 1 - portfolio/js/app.js
+++ Project 1 - portfolio/js/app.js
@@ -5,4 +5,16 @@ $(document).ready(function () {
     appendDots: ".slider-dots",
     dotsClass: "dots",
   });
+
+  let hemberger = document.querySelector(".hemberger");
+  let times = document.querySelector(".times");
+  let mobile_nav = document.querySelector(".mobile-nav");
+
+  hemberger.addEventListener("click", function () {
+    mobile_nav.classList.add("open");
+  });
+
+  times.addEventListener("click", function () {
+    mobile_nav.classList.remove("open");
+  });
 });
