0dd082678be88714b72a1b339ed6edb8493920b0
Muhammad Bilal <muhammadbilal254542@gmail.com>
2023-06-26 19:29:34 +0500

    Day 4 Hidden Search bar

---
+++ Traversery 50 Days challenege/Day 4 - Search Widget/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+  console.log("hello");
+});
